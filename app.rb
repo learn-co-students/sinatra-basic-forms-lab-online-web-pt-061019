16c90c4f4a4c84f20a593e75bc81145320046a1f
Daniel  Bovington <dbovington@daniels-imac.home>
2019-09-20 12:40:37 +0100

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+
+    erb :index
+  end
+
+  post '/puppy' do
+    @dog = params
+
+    erb :display_puppy
+  end
+
+  get '/new' do
+
+    erb :create_puppy
+  end
+
 end
