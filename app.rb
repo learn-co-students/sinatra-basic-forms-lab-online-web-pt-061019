86ff95fcca27889da3d4026ea8ca2781afab962f
Oscar <shoudto@gmail.com>
2019-09-26 21:03:20 -0400

    Done.

--- app.rb
+++ app.rb
@@ -7,8 +7,15 @@ class App < Sinatra::Base
     end 
 
     get '/new' do 
+        erb :create_puppy
     end
 
     post '/puppy' do 
+
+        @name = params[:name]
+        @breed = params[:breed]
+        @age = params[:age]
+
+        erb :display_puppy
     end
 end
